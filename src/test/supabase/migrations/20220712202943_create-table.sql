bb4c6a1fbe4d913581beafd07879338e81d71de0
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2022-07-23 01:17:57 +0200

    Add supabase test environment

---
+++ src/test/supabase/migrations/20220712202943_create-table.sql
@@ -0,0 +1,19 @@
+create table vehicles
+(
+    id      serial  not null
+        constraint vehicles_pk
+            primary key,
+    user_id uuid    not null
+        default auth.uid()
+        constraint vehicles_user_id_fk
+            references auth.users (id),
+    name    varchar not null
+        default ''
+);
+
+alter table vehicles
+    enable row level security;
+
+create policy vehicles_user_id_policy on vehicles
+    using (auth.uid() = user_id)
+    with check (auth.uid() = user_id);
